7f56186a7017be87ecbe458260d75e0053e9f1f2
RafaelElGon <104838747+RafaelElGon@users.noreply.github.com>
2023-06-01 11:31:18 -0300

    Update HeavyArmor.java

--- ProjetoPOO/src/main/java/Equipments/Armor/HeavyArmor.java
+++ ProjetoPOO/src/main/java/Equipments/Armor/HeavyArmor.java
@@ -2,7 +2,7 @@ package Equipments.Armor;
 
 public class HeavyArmor extends Armor{
     private static final int defense = 10;
-    private static int weight = (int) ((1 + Math.random() * 8) + (1 + Math.random() * 6));
+    private static int weight = (int) ((1 + Math.random() * 8) + (1 + Math.random() * 6));//weight is the result of 2 dices (1d8 + 1d6), measured in kilograms
 
     public HeavyArmor(String armorName) {
         super(armorName, defense, weight);
